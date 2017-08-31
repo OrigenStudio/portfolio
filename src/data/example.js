62b88633a00a1dd0970990a23b31fd226b02d4ae
PolGuixe <pol@zetoff.com>
2017-08-31 21:32:19 +0200

    WIP

---
+++ src/data/example.js
@@ -0,0 +1,8 @@
+export const data = {
+  title: 'Choropleth on d3v4',
+  written: '2017-05-04',
+  layoutType: 'post',
+  path: 'choropleth-on-d3v4',
+  category: 'data science',
+  description: 'Things about the choropleth.',
+};
