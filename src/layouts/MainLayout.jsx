032361ccde014861e0a4fdf108725ea3050274cf
PolGuixe <pol@zetoff.com>
2017-08-31 19:06:19 +0200

    updated MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,10 +3,27 @@ import PropTypes from 'prop-types';
 import { MuiThemeProvider } from 'material-ui/styles';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
+import { emojify } from 'react-emojione';
 
 import theme from '../config/theme';
 
 import Layout from '../components/Layout';
+import BasicFooter from '../components/FooterTemplates/BasicFooter';
+import { logo } from '../images';
+
+// TODO move somewhere else
+const smallMessage = `Built with
+  ${emojify(':heart_eyes:', { output: 'unicode' })}
+    in house`;
+
+const bigMessage = "Let's work together!";
+
+// TODO get from data
+const links = [
+  { label: 'Services', href: '#' },
+  { label: 'Contact', href: '#' },
+  { label: 'Projects', href: '#' },
+];
 
 // TODO convert to class
 const TemplateWrapper = ({ children, data }) => (
@@ -19,7 +36,21 @@ const TemplateWrapper = ({ children, data }) => (
           { name: 'keywords', content: 'sample, something' }, // TODO add SEO
         ]}
       />
-      <Layout title={data.site.siteMetadata.title} navbarPostion= "fixed" stickyFooter>
+      <Layout
+        title={data.site.siteMetadata.title}
+        logo={logo}
+        navbarPostion="fixed"
+        footerContent={
+          <BasicFooter
+            title={data.site.siteMetadata.title}
+            logo={logo}
+            smallMessage={smallMessage}
+            bigMessage={bigMessage}
+            links={links}
+          />
+        }
+        stickyFooter
+      >
         <div
           style={{
             margin: '0 auto',
