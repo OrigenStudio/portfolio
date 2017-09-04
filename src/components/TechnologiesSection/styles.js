6dbc0277f53947ef4dc9df52b5ffe15a9a1c2adf
PolGuixe <pol@zetoff.com>
2017-09-04 18:06:46 +0200

    added technologies section

---
+++ src/components/TechnologiesSection/styles.js
@@ -0,0 +1,10 @@
+const styles = theme => ({
+  wrapper: {
+    backgroundColor: theme.palette.grey[50],
+  },
+  image: {
+    height: '60px',
+  },
+});
+
+export default styles;
--- src/hidden/LandingPage/LandingPage.jsx
+++ src/hidden/LandingPage/LandingPage.jsx
@@ -4,6 +4,8 @@ import { withStyles } from 'material-ui/styles';
 
 import Services from '../../components/Services';
 import ContactUsSection from '../../components/ContactUsSection';
+import ProjectsSection from '../../components/ProjectsSection';
+import TechnologiesSection from '../../components/TechnologiesSection';
 
 import styles from './styles';
 
@@ -15,6 +17,8 @@ const IndexPage = ({ classes }) => (
     <Link to="/page-2/">Go to page 2</Link>
     <Services />
     <ContactUsSection />
+    <ProjectsSection />
+    <TechnologiesSection />
   </div>
 );
 
