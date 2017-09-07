20a19bd32027444bc8824e0747bac67656930ff7
PolGuixe <pol@zetoff.com>
2017-09-07 14:14:51 +0200

    added packages for data sourcing and transforming

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,33 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              // Remove the default behavior of adding a link to each
+              // image.
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-transformer-sharp',
+    {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/config/typography.js',
