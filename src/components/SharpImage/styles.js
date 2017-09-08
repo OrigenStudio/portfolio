7da281e6ef6d286c391efa2885b992c326a2482f
PolGuixe <pol@zetoff.com>
2017-09-08 18:08:05 +0200

    created a SharpImage components
    makes it easier to use sharp images from Gatsby plugin

---
+++ src/components/SharpImage/styles.js
@@ -0,0 +1,14 @@
+const styles = {
+  img: {
+    display: 'block',
+    objectFit: 'scale-down',
+    width: '100%',
+    height: 'auto',
+  },
+  wrapper: {
+    width: 'auto',
+    height: 'auto',
+  },
+};
+
+export default styles;
