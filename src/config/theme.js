6ef56b7fda9820b8227a413cda6c2f4f792dfdb0
PolGuixe <pol.guixe@gmail.com>
2018-05-15 18:46:31 +0200

    updated theme palette

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,19 +1,35 @@
 // @flow weak
 
 import { createMuiTheme } from 'material-ui/styles';
-import teal from 'material-ui/colors/teal';
-
-import alizarin from './colors/alizarin';
 
 import { fontFamily } from './typography';
 
 const palette = {
-  primary: alizarin,
+  primary: {
+    main: '#2FA7CD',
+  },
   secondary: {
-    ...teal,
+    main: '#E72476',
   },
   background: {
     appBar: '#3A3A3A',
+  },
+  other: {
+    darkBlue: '#333784',
+    lightGrey: '#F0F0F0',
+    darkGrey: '#18232B',
+    veryDarkGrey: '#0C1821',
+  },
+  gradients: {
+    primary: {
+      background: '#340e4f',
+      background: '-moz - linear - gradient(left, #340e4f 0 %, #f91275 100 %)',
+      background:
+        '-webkit - linear - gradient(left, #340e4f 0 %, #f91275 100 %)',
+      background: 'linear - gradient(to right, #340e4f 0 %, #f91275 100 %)',
+      filter:
+        "progid:DXImageTransform.Microsoft.gradient(startColorstr='#340e4f', endColorstr = '#f91275', GradientType = 1)",
+    },
   },
 };
 
