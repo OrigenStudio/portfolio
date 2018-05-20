411c57d231c17cd3be3c14fe001b6e6dc4e927f1
PolGuixe <pol.guixe@gmail.com>
2018-05-20 18:01:42 +0200

    typography font size changes depending screen size

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,8 +1,9 @@
 // @flow weak
-
+import each from 'lodash/each';
 import { createMuiTheme } from 'material-ui/styles';
 
-import { fontFamily , titlesFontFamily} from './typography';
+import { fontFamily, titlesFontFamily } from './typography';
+import { isObject } from 'util';
 
 const palette = {
   primary: {
@@ -27,7 +28,8 @@ const palette = {
   gradients: {
     primary: {
       backgroundImage: '#340e4f',
-      backgroundImage: '-moz - linear - gradient(left, #340e4f 0%, #f91275 100%)',
+      backgroundImage:
+        '-moz - linear - gradient(left, #340e4f 0%, #f91275 100%)',
       backgroundImage:
         '-webkit - linear - gradient(left, #340e4f 0%, #f91275 100%)',
       backgroundImage: 'linear-gradient(to right, #340e4f 0%, #f91275 100%)',
@@ -38,39 +40,39 @@ const palette = {
 };
 
 const typography = {
-  display4:{
+  display4: {
     fontFamily: titlesFontFamily,
     fontWeight: 700,
   },
-  display3:{
+  display3: {
     fontFamily: titlesFontFamily,
     fontWeight: 700,
   },
-  display2:{
+  display2: {
     fontFamily: titlesFontFamily,
     fontWeight: 700,
   },
-  display1:{
+  display1: {
     fontFamily: titlesFontFamily,
     fontWeight: 700,
   },
-  headline:{
+  headline: {
     fontFamily,
     fontWeight: 300,
   },
-  title:{
+  title: {
     fontFamily,
     fontWeight: 300,
   },
-  subheading:{
+  subheading: {
     fontFamily,
     fontWeight: 300,
   },
-  body1:{
+  body1: {
     fontFamily,
     fontWeight: 300,
   },
-  body2:{
+  body2: {
     fontFamily,
     fontWeight: 300,
   },
@@ -79,6 +81,33 @@ const typography = {
 const theme = createMuiTheme({
   palette,
   typography,
+});
+
+const coef = 0.1;
+const modifyRem = (value, coef) => {
+  return `${parseFloat(value) * (1 + coef)}rem`;
+};
+
+each(theme.typography, (variant, variantName) => {
+  if (typeof variant !== 'object') {
+    return variant;
+  }
+  theme.typography[variantName] = {
+    ...variant,
+    fontSize: modifyRem(variant.fontSize, -coef * 5),
+    [theme.breakpoints.up('sm')]: {
+      fontSize: modifyRem(variant.fontSize, -coef * 2.5),
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: modifyRem(variant.fontSize, -coef * 1),
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: modifyRem(variant.fontSize, 0),
+    },
+    [theme.breakpoints.up('xl')]: {
+      fontSize: modifyRem(variant.fontSize, coef),
+    },
+  };
 });
 
 export default theme;
