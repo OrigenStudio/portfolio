1ce2fdd32391717aefc47b208e79598590a781a6
Cristina Prim <cris@origen.studio>
2018-05-22 13:48:40 +0200

    Changed light position

--- src/components/ThreeDSectionTest2/threejs/GeneralLights.js
+++ src/components/ThreeDSectionTest2/threejs/GeneralLights.js
@@ -7,9 +7,9 @@ export default scene => {
     lights[0] = new THREE.DirectionalLight(0x9A9AC3, 1);
     lights[0].position.set(1, 0, 0);
     lights[1] = new THREE.DirectionalLight(0xFF0080, 1);
-    lights[1].position.set(0.75, 1, 0.5);
+    lights[1].position.set(1.00, 1, 0.5);
     lights[2] = new THREE.DirectionalLight(0x343784, 1);
-    lights[2].position.set(-0.75, -1, 0.5);
+    lights[2].position.set(-1.00, -1, 0.8);
     scene.add(lights[0]);
     scene.add(lights[1]);
     scene.add(lights[2]);
