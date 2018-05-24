a6388f61fef928d20ab7540709f46d91434377ff
PolGuixe <pol.guixe@gmail.com>
2018-05-24 17:23:03 +0200

    tried with scroll trigger

--- src/hidden/LandingPage/LandingPage.jsx
+++ src/hidden/LandingPage/LandingPage.jsx
@@ -1,5 +1,9 @@
 import { withStyles } from '@material-ui/core/styles';
 import React from 'react';
+import ScrollTrigger from 'react-scroll-trigger';
+import AppBar from '@material-ui/core/AppBar';
+import { BasicAppBar } from 'material-ui-layout'; 
+
 import ContactUsSection from '../../components/ContactUsSection';
 import LandingSection from '../../components/LandingSection';
 import IntroSection from '../../components/IntroSection';
@@ -8,14 +12,31 @@ import Services from '../../components/Services';
 import TechnologiesSection from '../../components/TechnologiesSection';
 import styles from './styles';
 
-const IndexPage = ({ classes, data }) => (
-  <div>
-    <LandingSection />
-    <IntroSection />
-    <Services />
-    <QualitySection />
-    <ContactUsSection />
-  </div>
-);
+import { logo } from '../../images'; 
 
+
+class IndexPage extends React.Component {
+  setAppBarVisibility = () => {
+    console.log('entered');
+
+    this.setState({ appBarVisibility: true });
+  };
+
+  render() {
+    const { classes, data } = this.props;
+
+    return <div>
+        <AppBar className={classes.appbar}>
+          <BasicAppBar title="Origen" logo={logo} />
+        </AppBar>
+        <LandingSection />
+          <IntroSection />
+          <Services />
+        <ScrollTrigger onEnter={this.setAppBarVisibility} triggerOnLoad={false}>
+          <QualitySection />
+          <ContactUsSection />
+        </ScrollTrigger>
+      </div>;
+  }
+}
 export default withStyles(styles)(IndexPage);
