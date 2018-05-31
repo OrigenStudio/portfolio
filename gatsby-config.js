a2d647c3de29b6c932ad03c4e8ce29c2dc86d402
PolGuixe <pol.guixe@gmail.com>
2018-05-31 19:52:28 +0200

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,5 +56,19 @@ module.exports = {
         appId: '24wpazn5b9fg',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ],
 };
