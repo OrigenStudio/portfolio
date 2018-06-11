edb264f20287ef5769b12bb5f9458a5d07992789
Cristina Prim <cris@origen.studio>
2018-06-11 12:59:12 +0200

    New padding values

--- src/components/Padding/styles.js
+++ src/components/Padding/styles.js
@@ -4,17 +4,17 @@ const styles = theme => ({
   },
   [theme.breakpoints.up('sm')]: {
     wrapper: {
-      padding: `${theme.spacing.unit * 8}px ${theme.spacing.unit * 6}px`,
+      padding: `${theme.spacing.unit * 6}px ${theme.spacing.unit * 4}px`,
     },
   },
   [theme.breakpoints.up('md')]: {
     wrapper: {
-      padding: `${theme.spacing.unit * 12}px ${theme.spacing.unit * 10}px`,
+      padding: `${theme.spacing.unit * 8}px ${theme.spacing.unit * 6}px`,
     },
   },
   [theme.breakpoints.up('lg')]: {
     wrapper: {
-      padding: `${theme.spacing.unit * 16}px ${theme.spacing.unit * 12}px`,
+      padding: `${theme.spacing.unit * 12}px ${theme.spacing.unit * 10}px`,
     },
   },
 });
