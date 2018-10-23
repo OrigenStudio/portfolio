d00d2f9ae3308bba7d40db43392e075a83e5e214
Cristina Prim <cris@origen.studio>
2018-10-23 11:44:56 +0200

    New padding values

--- src/components/Padding/styles.js
+++ src/components/Padding/styles.js
@@ -1,20 +1,20 @@
 const styles = theme => ({
   wrapper: {
-    padding: `${theme.spacing.unit * 4}px ${theme.spacing.unit * 2}px`,
+    padding: `${theme.spacing.unit * 8}px ${theme.spacing.unit * 3}px`,
   },
   [theme.breakpoints.up('sm')]: {
     wrapper: {
-      padding: `${theme.spacing.unit * 8}px ${theme.spacing.unit * 6}px`,
+      padding: `${theme.spacing.unit * 12}px ${theme.spacing.unit * 8}px`,
     },
   },
   [theme.breakpoints.up('md')]: {
     wrapper: {
-      padding: `${theme.spacing.unit * 12}px ${theme.spacing.unit * 10}px`,
+      padding: `${theme.spacing.unit * 18}px ${theme.spacing.unit * 12}px`,
     },
   },
   [theme.breakpoints.up('lg')]: {
     wrapper: {
-      padding: `${theme.spacing.unit * 16}px ${theme.spacing.unit * 12}px`,
+      padding: `${theme.spacing.unit * 20}px ${theme.spacing.unit * 14}px`,
     },
   },
 });
