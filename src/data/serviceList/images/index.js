0b6e60ac19b2586f2bb1a68dae8d5141be16fec5
Cristina Prim <cris@origen.studio>
2019-01-16 16:25:04 +0100

    Added new background images

--- src/data/serviceList/images/index.js
+++ src/data/serviceList/images/index.js
@@ -1,23 +1,11 @@
-import threeD from './3dmodels.jpg';
-import analytics from './analytics.jpg';
-import development from './development.jpg';
-import marketing from './marketing.jpg';
-import mvp from './mvp.jpg';
-import native from './native.jpg';
-import uxui from './uxui.jpg';
-import deployment from './deployment.jpg';
-import data from './data.jpg';
+import back01 from './back01.png';
+import back02 from './back02.png';
+import back03 from './back03.png';
 
 const images = {
-  threeD,
-  analytics,
-  development,
-  marketing,
-  mvp,
-  native,
-  uxui,
-  deployment,
-  data
+  back01,
+  back02,
+  back03,
 };
 
 export default images;
