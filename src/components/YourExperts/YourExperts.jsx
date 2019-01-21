472d3c3be85b5da89be98451ce277c0b89e2ac1f
Cristina Prim <cris@origen.studio>
2019-01-21 10:27:51 +0100

    Updated YourExperts component structure

--- src/components/YourExperts/YourExperts.jsx
+++ src/components/YourExperts/YourExperts.jsx
@@ -4,30 +4,34 @@ import React from 'react';
 import TextLoop from 'react-text-loop';
 import styles from './styles';
 
+import Section from '../Section';
+
 class YourExperts extends React.PureComponent {
   render() {
     const { classes, typographyVariant } = this.props;
     return (
-      <div className={classes.wrapper}>
+      <Section className={classes.wrapper}>
         <div className={classes.textWrapper}>
           <Typography
-            variant={typographyVariant || "display2"}
+            variant={typographyVariant || 'display3'}
             color="inherit"
             className={classes.title}
           >
-            <span>WE ARE YOUR <br/> EXPERTS IN </span>
-            <TextLoop speed={1500}>
-              <span>React</span>
-              <span>React-Native</span>
-              <span>GraphQL</span>
-              <span>MongoDB</span>
-              <span>MeteorJS</span>
-              <span>ThreeJS</span>
-              <span>WebGL</span>
+            <span>WE ARE YOUR EXPERTS IN </span>
+            <TextLoop speed={2000}>
+              <span className={classes.textLoop}>React</span>
+              <span className={classes.textLoop}>React-Native</span>
+              <span className={classes.textLoop}>GraphQL</span>
+              <span className={classes.textLoop}>MongoDB</span>
+              <span className={classes.textLoop}>MeteorJS</span>
+              <span className={classes.textLoop}>ThreeJS</span>
+              <span className={classes.textLoop}>WebGL</span>
             </TextLoop>
+            <br />
+            <span>LOREM IPSUM DOLOR SIT AMET</span>
           </Typography>
         </div>
-      </div>
+      </Section>
     );
   }
 }
