85298050dadbb91829d12974033d8b35c39ff427
Cristina Prim <cris@origen.studio>
2019-01-23 13:57:23 +0100

    Added Highlights data config

---
+++ src/data/commercialWebList/commercialWebList.js
@@ -0,0 +1,36 @@
+import images from './images';
+
+export const commercialWebList1= [
+  {
+    image: images.design,
+    title: 'Design title',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae mi iaculis neque vestibulum tincidunt ut ac orci. Nam dolor libero, ullamcorper sed blandit ut, efficitur a odio. Praesent consequat leo urna, mollis rhoncus augue dignissim eu.',
+    reverseOrder: false,
+  },
+  {
+    image: images.responsive,
+    title: 'Responsive title',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae mi iaculis neque vestibulum tincidunt ut ac orci. Nam dolor libero, ullamcorper sed blandit ut, efficitur a odio. Praesent consequat leo urna, mollis rhoncus augue dignissim eu.',
+    reverseOrder: true,
+  },
+];
+
+export const commercialWebList2= [
+  {
+    image: images.design,
+    title: 'Fast title',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae mi iaculis neque vestibulum tincidunt ut ac orci. Nam dolor libero, ullamcorper sed blandit ut, efficitur a odio. Praesent consequat leo urna, mollis rhoncus augue dignissim eu.',
+    reverseOrder: false,
+  },
+  {
+    image: images.responsive,
+    title: 'Scalable title',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae mi iaculis neque vestibulum tincidunt ut ac orci. Nam dolor libero, ullamcorper sed blandit ut, efficitur a odio. Praesent consequat leo urna, mollis rhoncus augue dignissim eu.',
+    reverseOrder: true,
+  },
+];
+
---
+++ src/data/commercialWebList/images/index.js
@@ -0,0 +1,7 @@
+import design from './design.png';
+import responsive from './responsive.png';
+
+export default {
+    design,
+    responsive
+};
