76336eb410046e756cbdc1b356b889c6b17a5d63
Cristina Prim <cris@origen.studio>
2019-01-23 13:59:40 +0100

    Created the map system

--- src/pages/commercial-web.jsx
+++ src/pages/commercial-web.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import map from 'lodash/map';
 
 import PageTitleIterator from '../components/PageTitleIterator';
+import HighlightsBackground from '../components/Highlights/HighlightsBackground';
+import HighlightsBase from '../components/Highlights/HighlightsBase';
+import {commercialWebList1, commercialWebList2} from '../data/commercialWebList';
 
-const CommercialWeb = {
+const CommercialWebTitleInfo = {
   headline: 'Commercial Web',
   text1: 'YOUR NEXT ',
   text2: 'WEBSITE WILL BE RESPONSIVE, FAST, SCALABLE AND SAFE',
@@ -13,8 +17,14 @@ class CommercialWebPage extends React.PureComponent {
   render() {
     const { data } = this.props;
     return (
-      <div style={{ width: '100%'}}>
-        <PageTitleIterator {...CommercialWeb} />
+      <div style={{ width: '100%' }}>
+        <PageTitleIterator {...CommercialWebTitleInfo} />
+        <HighlightsBackground>
+          {map(commercialWebList1, item => <HighlightsBase {...item} />)}
+        </HighlightsBackground>
+        <HighlightsBackground>
+          {map(commercialWebList2, item => <HighlightsBase {...item} />)}
+        </HighlightsBackground>
       </div>
     );
   }
