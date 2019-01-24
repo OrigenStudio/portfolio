a56e324290346fd83a93e9e7bb8222f7c8b3d856
Pere Prim <pereprimcarol@gmail.com>
2019-01-24 12:57:30 +0100

    Created map and data system

---
+++ src/data/auditList/auditList.js
@@ -0,0 +1,24 @@
+const audits = [
+    {
+      description:
+        'La decisión de tirar la toalla o seguir adelante es tuya. Así que envíate mensajes positivos para poder seguir avanzando, porque de lo contrario no lograrás lo que te propones.',
+    },
+    {
+        description:
+          'Los fracasos son una oportunidad inmejorable para aprender, pues si te lo tomas como una experiencia de aprendizaje, posiblemente no volverás a cometer los mismos errores que en el pasado.',
+    },
+    {
+        description:
+          'Los fracasos son una oportunidad inmejorable para aprender, pues si te lo tomas como una experiencia de aprendizaje, posiblemente no volverás a cometer los mismos errores que en el pasado.',
+    },
+    {
+        description:
+          'Los fracasos son una oportunidad inmejorable para aprender, pues si te lo tomas como una experiencia de aprendizaje, posiblemente no volverás a cometer los mismos errores que en el pasado.',
+    },
+    {
+        description:
+          'Los fracasos son una oportunidad inmejorable para aprender, pues si te lo tomas como una experiencia de aprendizaje, posiblemente no volverás a cometer los mismos errores que en el pasado.',
+    },
+  ];
+  
+  export default audits;
